8710990d180d433a84e163cbc4486bd8b087e642
25ec007-akshaya <25ec007@rmkcet.ac.in>
2026-06-10 14:00:44 +0530

    Update and rename code-8 longest common substring.java to code8-longest common substring.java

--- code8-longest common substring.java
+++ code8-longest common substring.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String s1 = sc.next();
-        String s2 = sc.next();
-
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
         int n = s1.length();
         int m = s2.length();
-
-        int[][] dp = new int[m + 1][n + 1];
-
+        int[][] dp = new int[n + 1][m + 1];
         int maxLength = 0;
-
-        for (int row = 1; row <= m; row++) {
-            for (int col = 1; col <= n; col++) {
-
-                if (s2.charAt(row - 1) == s1.charAt(col - 1)) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= m; col++) {
+                if (s1.charAt(row - 1) == s2.charAt(col - 1)) {
                     dp[row][col] = dp[row - 1][col - 1] + 1;
-
                     if (dp[row][col] > maxLength) {
                         maxLength = dp[row][col];
                     }
                 } else {
+
                     dp[row][col] = 0;
                 }
             }
